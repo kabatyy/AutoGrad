50fb24b9173010ea794d72d22e5be45f7b5a1201
OswaldCc <sam.kabati@gmail.com>
2024-04-02 15:30:28 +0300

    feat: completed addition functionality

--- autograd/engine.py
+++ autograd/engine.py
@@ -7,8 +7,9 @@ class Value:
     def __repr__(self):
         return f"Value(data={self.data})"
     
-    def __str__(self):
-        return f"{self.data}"
     def __add__(self,other):
+        other=other if isinstance(other,Value) else Value(other)
         out=Value(self.data+other.data,(self,other),'+')
         return out 
+    def __radd__(self,other):
+        return self+other
--- autograd/test_engine.py
+++ autograd/test_engine.py
@@ -1,9 +1,18 @@
 from engine import Value 
 
 def test_ops():
+    #normal add 
     a=Value(2.2)
     b=Value(4.5)
-    return a+b
+    c=a+b
+    print(c)
+    #add Value+constant
+    d=Value(2.0)+5
+    print(d)
+    #reverse add
+    e=5+Value(3.0)
+    print(e)
+
+test_ops()
+
      
-c=test_ops()
-print(c._prev)
