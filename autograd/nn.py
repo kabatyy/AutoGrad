e9fe633da5d839f80cc53817dad2b2e4aef11359
OswaldCc <sam.kabati@gmail.com>
2024-04-09 16:54:35 +0300

    wrote the multilayer perceptron

--- autograd/nn.py
+++ autograd/nn.py
@@ -21,4 +21,19 @@ class Layer:
         for neuron in self.neurons:
             ps=neuron.parameters()
             params.extend(ps)
+        return params 
+class MLP:
+    def __init__(self,nin,nouts):
+        sz=[nin]+nouts 
+        self.layers=[Layer(sz[i],sz[i+1]) for i in range(len(nouts))]
+    def __call__(self,x):
+        for layer in self.layers:
+            x=layer(x)
+        return x
+    def parameters(self):
+        params=[]
+        for layer in self.layers:
+            ps=layer.parameters()
+            params.extend(ps)
         return params
+    
