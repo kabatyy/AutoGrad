94897cebfa16d63622004443b5ecabcc402a1353
OswaldCc <sam.kabati@gmail.com>
2024-09-20 15:48:50 +0300

    feat: implement mul operator

--- autograd/tensor_engine.py
+++ autograd/tensor_engine.py
@@ -31,5 +31,15 @@ class Tensor:
     def __radd__(self, other):
         return self + other
     
+    @ensure_2d_tensor
+    def __mul__(self, other):
+        out = Tensor(self.data * other.data, (self, other), '*')
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+        return out
     
+    def __rmul__(self, other):
+        return self * other
         
