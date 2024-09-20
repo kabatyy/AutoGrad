0c02c977d2d071755f2951822838fbf7c84c51b3
OswaldCc <sam.kabati@gmail.com>
2024-09-20 15:11:05 +0300

    feat: implement the addition operator

--- autograd/tensor_engine.py
+++ autograd/tensor_engine.py
@@ -9,10 +9,27 @@ def ensure_2d_tensor(func):
         return func(self, other)
     return wrapper
 class Tensor:
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = np.atleast_2d(np.array(data, dtype=np.float64))
         self.grad = np.zeros_like(self.data)
         self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
+    
+    def __repr__(self):
+        return f"Tensor(data={self.data}, grad={self.grad}, children={self._prev}, op={self._op})"
+    
+    @ensure_2d_tensor
+    def __add__(self, other):
+        out = Tensor(self.data + other.data, (self, other), '+')
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+        out._backward = _backward
+        return out
+    
+    def __radd__(self, other):
+        return self + other
+    
+    
         
