:fix: sum gradient across broadcasted dims in binary ops

autograd/core/engine.py
@@ -1,7 +1,8 @@
 import math
 import numpy as np
 from functools import wraps
-
+from  .utils import broadcast_axis
+from typing import *
 class Value:
     def __init__(self,data,_children=(),_op='',label=''):
         self.data=data
@@ -82,14 +83,6 @@ class Value:
             node._backward()
 
 
-def ensure_2d_tensor(func):
-    @wraps(func)
-    def wrapper(self, other):
-        if not isinstance(other, Tensor):
-            other = Tensor(other)
-        return func(self, other)
-    return wrapper
-
 class Tensor:
     def __init__(self, data, _children=(), _op='', label=''):
         self.data = np.atleast_2d(np.array(data, dtype=np.float64))
@@ -97,14 +90,35 @@ class Tensor:
         self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
-        self.shape = np.shape(self)
+        self.shape = np.shape(self.data)
     
     def __repr__(self):
         return f"Tensor(data={self.data}, grad={self.grad})"
     
-    @ensure_2d_tensor
+    def broadcast_to(self, shape: Tuple[int]):
+        new_shape = np.broadcast_to(self.data, shape)
+        out = Tensor(new_shape, (self,), f'broadcast_to {shape}')
+        broadcasted_axes = broadcast_axis(self.shape, shape)[0]
+        
+        def _backward():
+            self.grad += np.sum(out.grad, axis=broadcasted_axes, keepdims=True)
+        out._backward = _backward
+        return out
+   
+    def _preprocess_binop(self, other):
+        if not isinstance(other, Tensor):
+            other = Tensor(other)
+        if self.shape == other.shape:
+            return self, other
+        else:
+            broadcast_shape = np.broadcast_shapes(self.shape, other.shape)
+            self, other = self.broadcast_to(broadcast_shape), other.broadcast_to(broadcast_shape)
+            return self, other
+        
     def __add__(self, other):
+        self, other = self._preprocess_binop(other)
         out = Tensor(self.data + other.data, (self, other), '+')
+       
         def _backward():
             self.grad += out.grad
             other.grad += out.grad
@@ -114,9 +128,11 @@ class Tensor:
     def __radd__(self, other):
         return self + other
     
-    @ensure_2d_tensor
+
     def __mul__(self, other):
+        self, other = self._preprocess_binop(other)
         out = Tensor(self.data * other.data, (self, other), '*')
+        
         def _backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
@@ -129,13 +145,13 @@ class Tensor:
     def __neg__(self):
         return self * - 1
     
-    @ensure_2d_tensor
     def __sub__(self, other):
         return self + (-other)
     
     def __pow__(self, other):
         assert isinstance(other,(int, float)), "power must be a scalar"
         out = Tensor(self.data ** other, (self,), f'**{other}')
+       
         def _backward():
             self.grad += other * self.data ** (other - 1) * out.grad
         out._backward = _backward
@@ -144,9 +160,11 @@ class Tensor:
     def __truediv__(self, other):
         return self * (other ** - 1)
     
-    @ensure_2d_tensor
     def __matmul__(self, other):
+        if not isinstance(other, Tensor):
+            other = Tensor(other)
         out = Tensor(self.data @ other.data, (self, other), '@')
+        
         def _backward():
             self.grad += out.grad @ other.data.T
             other.grad += self.data.T @ out.grad
@@ -156,6 +174,7 @@ class Tensor:
     def exp(self):
         x = np.exp(self.data)
         out = Tensor(x, (self,), 'exp')
+       
         def _backward():
             self.grad += out.data * out.grad
         out._backward = _backward
@@ -169,9 +188,18 @@ class Tensor:
         out._backward = _backward
         return out 
     
+    def mean(self, axis=None, keepdims=True):
+        if axis:
+            n = self.data.shape[axis]
+        else:
+            n = self.data.size 
+        out = (self.sum(axis=axis, keepdims=keepdims)) / n
+        return out 
+    
     def max(self, axis=None, keepdims=False):
         max_vals = np.max(self.data, axis=axis, keepdims=keepdims)
         out = Tensor(max_vals, (self,), 'max')
+        
         def _backward():
             grad_mask = (self.data == max_vals)
             num_maxes = grad_mask.sum(axis=axis, keepdims=keepdims)
@@ -184,6 +212,7 @@ class Tensor:
             x= self.data + 1e-12
             log = np.log(x)
             out = Tensor(log, (self,), 'log')
+            
             def _backward():
                 self.grad += (1 / x) * out.grad
             out._backward =_backward
@@ -193,6 +222,7 @@ class Tensor:
         x = self.data
         t = (np.exp(2 * x) - 1)/(np.exp(2 * x) + 1)
         out = Tensor(t, (self,), 'tanh')
+       
         def _backward():
             self.grad += (1 - t**2) * out.grad
         out._backward = _backward
@@ -200,6 +230,7 @@ class Tensor:
     
     def relu(self):
         out = Tensor(np.maximum(0, self.data), (self,), 'relu')
+       
         def _backward():
             self.grad += (out.data > 0).astype(np.float64) * out.grad
         out._backward = _backward
@@ -209,6 +240,7 @@ class Tensor:
         x = self.data
         s = (1 / (1 + np.exp(-x)))
         out = Tensor(s, (self,), 'sigmoid')
+        
         def _backward():
             self.grad += (s * (1 - s))  * out.grad
         out._backward = _backward
@@ -221,10 +253,9 @@ class Tensor:
         out = exps / sum_exps
         return out
    
-    @ensure_2d_tensor
-    def cross_entropy_loss(self, target):
+    def cross_entropy_loss(self, target, axis=None, keepdims=True):
         log_probs = self.log()
-        out = -((target * log_probs).sum())
+        out = -((target * log_probs).sum(axis=axis, keepdims=keepdims))
         return out 
     
     def backward(self):

autograd/tests/tensor_engine_test.py
@@ -100,13 +100,50 @@ def test_sigmoid():
      np.testing.assert_array_almost_equal(a.grad, s * (1 -s) * result.grad)
 
 def test_softmax():
-      a = Tensor([
+    a = Tensor([
         [0, -2],
         [3, 4]
     ])
-      result = a.softmax(axis=-1, keepdims=True)
-      x = torch.Tensor([[0, -2],[3, 4]])
-      s = torch.softmax(x, dim=-1)
-      np.testing.assert_array_almost_equal(result.data, s)
-    
-      
+    b = Tensor([
+        [-2],
+        [4]
+    ])
+    c = a + b  
+    x = torch.tensor([[0, -2], [3, 4]], dtype=torch.float32, requires_grad=True)
+    y = torch.tensor([[-2], [4]], dtype=torch.float32, requires_grad=True)
+    z = x + y  
+    result = c.softmax(axis=-1, keepdims=True) 
+    s = torch.softmax(z, dim=-1)  
+    np.testing.assert_array_almost_equal(result.data, s.detach().numpy(), decimal=5)
+    result.backward()  
+    grad_output = torch.ones_like(s)  
+    s.backward(grad_output)  
+    np.testing.assert_array_almost_equal(b.grad, y.grad.numpy(), decimal=5)
+    np.testing.assert_array_almost_equal(a.grad, x.grad.numpy(), decimal=5)
+
+def test_cross_entropy_loss():
+    a = Tensor([
+        [0, -2],
+        [3, 4]
+    ])
+    b = Tensor([
+        [-2],
+        [4]
+    ])
+    c = a + b  
+    target = Tensor([
+        [0, 1],
+        [1, 0]
+    ]) 
+    x = torch.tensor([[0, -2], [3, 4]], dtype=torch.float32, requires_grad=True)
+    y = torch.tensor([[-2], [4]], dtype=torch.float32, requires_grad=True)
+    z = x + y 
+    target_torch = torch.tensor([1, 0], dtype=torch.int64)  
+    result = c.softmax(axis=-1, keepdims=True)
+    loss = result.cross_entropy_loss(target, axis=-1)
+    torch_loss = torch.nn.functional.cross_entropy(z, target_torch)
+    np.testing.assert_almost_equal(loss.data, torch_loss.detach().numpy(), decimal=5)
+    loss.backward()
+    torch_loss.backward()
+    np.testing.assert_almost_equal(a.grad, x.grad.numpy(), decimal=5)
+    np.testing.assert_almost_equal(b.grad, y.grad.numpy(), decimal=5)